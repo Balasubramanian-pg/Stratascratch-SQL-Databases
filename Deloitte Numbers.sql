fb35fdc1fa611083693987a2451d710530d35d32
Balasubramanian PG <102461377+Balasubramanian-pg@users.noreply.github.com>
2026-02-01 22:41:36 +0530

    Auto-generated 300 rows of realistic data for deloitte_numbers

--- Deloitte Numbers.sql
+++ Deloitte Numbers.sql
@@ -1,2 +1,899 @@
 INSERT INTO deloitte_numbers (number)
-VALUES (1);
+VALUES ('compare');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('number');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('find');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('which');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('include');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('bag');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('end');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('party');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('way');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('course');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('level');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('attack');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('light');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('education');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('positive');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('can');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('write');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('arrive');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('particular');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('southern');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('this');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('pattern');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('recent');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('again');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('baby');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('compare');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('TV');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('build');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('because');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('high');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('tend');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('group');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('agree');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('level');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('begin');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('about');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('center');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('movement');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('mean');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('over');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('candidate');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('theory');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('six');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('natural');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hotel');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('member');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('candidate');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('off');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('success');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('experience');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('why');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('read');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('offer');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hot');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('write');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('guy');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('everything');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('let');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('edge');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('couple');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('keep');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('center');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('management');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('church');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('west');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('project');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('pretty');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('determine');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('figure');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('fill');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('themselves');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('father');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('group');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('culture');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('well');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('American');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('first');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('can');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('share');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('culture');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('past');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('scientist');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('bill');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('point');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('movie');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('guy');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('after');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('stuff');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('account');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('begin');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('in');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('actually');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('money');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('receive');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('east');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('its');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('for');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('simply');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('employee');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hold');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('good');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('common');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('drug');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('drug');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('represent');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('argue');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('fear');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('moment');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('above');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('full');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('story');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('development');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('program');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('foreign');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('reduce');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('win');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('she');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('without');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('against');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('year');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sport');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('suggest');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('state');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('movie');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('themselves');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('series');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('one');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('individual');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('happen');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('Democrat');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('establish');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('cause');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('without');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('right');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('face');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('would');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('performance');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('fear');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('particular');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('coach');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('light');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('save');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('about');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('financial');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('ask');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('director');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('site');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('read');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('address');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('own');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('this');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('brother');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('why');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('culture');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sound');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('scientist');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('during');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('town');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hand');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('painting');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('level');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('produce');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('manager');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('who');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('fact');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('their');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('nearly');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('relate');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('body');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('that');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('resource');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('call');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('far');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('soldier');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('leg');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('church');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('challenge');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('eye');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('yourself');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('several');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('usually');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('over');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('quite');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('day');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('successful');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('concern');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('little');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('positive');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('land');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('Congress');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('down');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sound');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('long');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('woman');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('free');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('others');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('catch');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sing');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('stage');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('Mrs');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('somebody');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('worry');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('rather');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('full');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('so');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('community');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('degree');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('up');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('condition');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('lawyer');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hospital');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('development');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('seek');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('much');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('a');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('involve');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('wish');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('spend');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sit');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('require');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('back');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('none');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('prove');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('debate');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('success');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('another');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('shake');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('deep');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('yes');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('by');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('standard');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('must');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('begin');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('newspaper');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('new');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('organization');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('week');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('up');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('part');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('everything');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('identify');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('seat');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('local');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('report');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('pattern');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('garden');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('by');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('no');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('change');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('garden');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('rate');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('point');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('music');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('cultural');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('laugh');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('movie');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('become');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('full');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('manager');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('environment');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('less');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('should');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('leave');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hold');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('contain');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('year');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('seven');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('go');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('with');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('himself');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('still');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('despite');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('fly');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('body');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('away');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('reflect');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('sign');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('guess');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('money');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('plan');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('situation');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('miss');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('believe');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('machine');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('thus');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('at');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('majority');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('hand');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('analysis');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('continue');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('course');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('military');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('foot');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('serious');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('never');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('need');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('alone');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('both');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('be');
+
+INSERT INTO deloitte_numbers (number)
+VALUES ('provide');
