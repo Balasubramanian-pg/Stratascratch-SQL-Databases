fbea2f48899728c05a59bcfe97fe11ea175bacdd
Balasubramanian PG <102461377+Balasubramanian-pg@users.noreply.github.com>
2026-02-01 22:44:53 +0530

    Auto-generated 300 rows of realistic data for online_promotions

--- Online Promotions.sql
+++ Online Promotions.sql
@@ -1,2 +1,899 @@
 INSERT INTO online_promotions (promotion_id)
 VALUES (1);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (2);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (3);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (4);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (5);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (6);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (7);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (8);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (9);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (10);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (11);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (12);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (13);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (14);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (15);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (16);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (17);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (18);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (19);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (20);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (21);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (22);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (23);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (24);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (25);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (26);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (27);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (28);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (29);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (30);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (31);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (32);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (33);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (34);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (35);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (36);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (37);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (38);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (39);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (40);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (41);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (42);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (43);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (44);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (45);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (46);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (47);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (48);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (49);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (50);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (51);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (52);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (53);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (54);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (55);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (56);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (57);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (58);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (59);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (60);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (61);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (62);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (63);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (64);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (65);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (66);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (67);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (68);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (69);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (70);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (71);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (72);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (73);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (74);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (75);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (76);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (77);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (78);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (79);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (80);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (81);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (82);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (83);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (84);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (85);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (86);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (87);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (88);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (89);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (90);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (91);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (92);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (93);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (94);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (95);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (96);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (97);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (98);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (99);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (100);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (101);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (102);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (103);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (104);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (105);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (106);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (107);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (108);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (109);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (110);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (111);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (112);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (113);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (114);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (115);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (116);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (117);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (118);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (119);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (120);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (121);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (122);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (123);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (124);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (125);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (126);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (127);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (128);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (129);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (130);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (131);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (132);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (133);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (134);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (135);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (136);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (137);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (138);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (139);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (140);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (141);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (142);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (143);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (144);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (145);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (146);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (147);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (148);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (149);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (150);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (151);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (152);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (153);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (154);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (155);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (156);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (157);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (158);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (159);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (160);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (161);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (162);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (163);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (164);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (165);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (166);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (167);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (168);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (169);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (170);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (171);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (172);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (173);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (174);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (175);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (176);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (177);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (178);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (179);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (180);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (181);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (182);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (183);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (184);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (185);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (186);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (187);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (188);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (189);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (190);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (191);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (192);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (193);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (194);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (195);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (196);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (197);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (198);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (199);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (200);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (201);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (202);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (203);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (204);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (205);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (206);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (207);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (208);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (209);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (210);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (211);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (212);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (213);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (214);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (215);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (216);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (217);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (218);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (219);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (220);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (221);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (222);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (223);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (224);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (225);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (226);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (227);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (228);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (229);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (230);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (231);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (232);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (233);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (234);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (235);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (236);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (237);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (238);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (239);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (240);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (241);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (242);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (243);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (244);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (245);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (246);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (247);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (248);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (249);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (250);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (251);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (252);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (253);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (254);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (255);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (256);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (257);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (258);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (259);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (260);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (261);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (262);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (263);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (264);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (265);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (266);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (267);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (268);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (269);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (270);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (271);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (272);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (273);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (274);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (275);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (276);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (277);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (278);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (279);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (280);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (281);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (282);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (283);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (284);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (285);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (286);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (287);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (288);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (289);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (290);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (291);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (292);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (293);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (294);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (295);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (296);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (297);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (298);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (299);
+
+INSERT INTO online_promotions (promotion_id)
+VALUES (300);
